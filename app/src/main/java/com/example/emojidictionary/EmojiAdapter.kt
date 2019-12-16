9756786dc202e25174abd2384fa455f2e39d32d9
lisabg <lisabgreenquist@gmail.com>
2019-12-16 21:29:00 +0100

    first commit

---
+++ app/src/main/java/com/example/emojidictionary/EmojiAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.emojidictionary
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
+
+class EmojiAdapter(val emojis: ArrayList<String>) : RecyclerView.Adapter<EmojiAdapter.TextHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextHolder {
+        return TextHolder(LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item_row, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return emojis.count()
+    }
+
+    override fun onBindViewHolder(holder: EmojiAdapter.TextHolder, position: Int) {
+        val emoji = emojis[position]
+        holder.bindEmoji(emoji)
+    }
+
+
+    class TextHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+
+        var view : View = v
+        var emoji : String = ""
+
+        fun bindEmoji(emoji:String) {
+            this.emoji = emoji
+            view.itemTextView.text = emoji
+        }
+
+        override fun onClick(p0: View?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
+
+}
