a37b19b573bd45e89c0be38b877d3d7d8394a2f8
lisabg <lisabgreenquist@gmail.com>
2019-12-17 15:09:19 +0100

    finished

--- app/src/main/java/com/example/emojidictionary/EmojiAdapter.kt
+++ app/src/main/java/com/example/emojidictionary/EmojiAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.emojidictionary
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.recyclerview_item_row.view.*
 
@@ -27,13 +29,23 @@ class EmojiAdapter(val emojis: ArrayList<String>) : RecyclerView.Adapter<EmojiAd
         var view : View = v
         var emoji : String = ""
 
+        init {
+            v.setOnClickListener(this)
+        }
+
         fun bindEmoji(emoji:String) {
             this.emoji = emoji
             view.itemTextView.text = emoji
         }
 
         override fun onClick(p0: View?) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+            val detailIntent = Intent(view.context, EmojiDetailActivity::class.java)
+            detailIntent.putExtra("emoji", emoji)
+            startActivity(view.context, detailIntent, null)
+
+
+
         }
 
     }
